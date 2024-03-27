6c77f29aaadcfb8d3d033e06c4c7506c8075fc74
MiheerTamkhane <sarangtamkhane10@gmail.com>
2024-03-27 22:11:55 +0530

    started working on Question2

--- src/components/cycleOne/Question2.tsx
+++ src/components/cycleOne/Question2.tsx
@@ -1,77 +1,44 @@
 import React, { useState } from "react";
 
-const todos = [
-  {
-    id: 1,
-    title: "NeoG assignments",
-    completed: false,
-  },
-  {
-    id: 2,
-    title: "Pair programming session",
-    completed: true,
-  },
-  {
-    id: 3,
-    title: "Project submission",
-    completed: false,
-  },
-  {
-    id: 4,
-    title: "Coding practice",
-    completed: true,
-  },
-];
-
 const Question2 = () => {
-  const [data, setData] = useState(todos);
-  function handleClick(flag: any): void {
-    if (flag === "all") {
-      setData(todos);
-    } else {
-      const filteredData = todos.filter((d) => flag === d.completed);
-      setData(filteredData);
-    }
-  }
   return (
     <div>
-      <ul>
-        <li> - Your challenge is to add a filter to do the following</li>
-        <li> - Show completed todos</li>
-        <li> - Show incomplete todos</li>
-        <li> - Show all todos</li>
-        <li>- The todos data structure is as follows</li>
-      </ul>
-      <div>
-        <button
-          onClick={() => {
-            handleClick("all");
-          }}
-        >
-          All
-        </button>
-        <button
-          onClick={() => {
-            handleClick(true);
-          }}
-        >
-          Completed
-        </button>
-        <button
-          onClick={() => {
-            handleClick(false);
-          }}
-        >
-          In-Completed
-        </button>
-      </div>
-      <ul>
-        {data.map((item) => (
-          <li>{item.title}</li>
-        ))}
-      </ul>
+      <TodoApp />
     </div>
   );
 };
 
 export default Question2;
+
+const TodoApp = () => {
+  const data = [
+    {
+      id: "c1",
+      brand: "Toyota",
+      make: "Corolla",
+      model: "2007",
+    },
+    {
+      id: "c2",
+      brand: "Maruti Suzuki",
+      make: "Omni",
+      model: "2003",
+    },
+    {
+      id: "c3",
+      brand: "Hyundai",
+      make: "Santro",
+      model: "2005",
+    },
+  ];
+  const [todos, setTodos] = useState(data);
+  return (
+    <>
+      {todos.map((todo) => (
+        <li>
+          {todo.brand} - {todo.make} - {todo.model}
+        </li>
+      ))}
+    </>
+  );
+};
