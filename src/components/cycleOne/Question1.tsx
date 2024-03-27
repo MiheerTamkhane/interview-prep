e6178bbbd62db692879d5719378a44c568ff697c
MiheerTamkhane <sarangtamkhane10@gmail.com>
2024-03-27 22:11:29 +0530

    Changed format of Question1

--- src/components/cycleOne/Question1.tsx
+++ src/components/cycleOne/Question1.tsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 
 const Question1 = () => {
+  return (
+    <div>
+      <FilterBrands />
+      <SortTodoList />
+    </div>
+  );
+};
+
+export default Question1;
+
+const FilterBrands = () => {
   const brands1 = [
     { id: "0a", brandName: "puma" },
     { id: "0b", brandName: "adiddas" },
@@ -32,13 +43,82 @@ const Question1 = () => {
       />
       <ul>
         {filteredBrands?.map((item) => (
-          <>
-            <li>{item.brandName}</li>
-          </>
+          <li>{item.brandName}</li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default Question1;
+const SortTodoList = () => {
+  const todos = [
+    {
+      id: 1,
+      title: "NeoG assignments",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Pair programming session",
+      completed: true,
+    },
+    {
+      id: 3,
+      title: "Project submission",
+      completed: false,
+    },
+    {
+      id: 4,
+      title: "Coding practice",
+      completed: true,
+    },
+  ];
+  const [data, setData] = useState(todos);
+  function handleClick(flag: any): void {
+    if (flag === "all") {
+      setData(todos);
+    } else {
+      const filteredData = todos.filter((d) => flag === d.completed);
+      setData(filteredData);
+    }
+  }
+  return (
+    <div>
+      <ul>
+        <li> - Your challenge is to add a filter to do the following</li>
+        <li> - Show completed todos</li>
+        <li> - Show incomplete todos</li>
+        <li> - Show all todos</li>
+        <li>- The todos data structure is as follows</li>
+      </ul>
+      <div>
+        <button
+          onClick={() => {
+            handleClick("all");
+          }}
+        >
+          All
+        </button>
+        <button
+          onClick={() => {
+            handleClick(true);
+          }}
+        >
+          Completed
+        </button>
+        <button
+          onClick={() => {
+            handleClick(false);
+          }}
+        >
+          In-Completed
+        </button>
+      </div>
+      <ul>
+        {data.map((item) => (
+          <li>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
