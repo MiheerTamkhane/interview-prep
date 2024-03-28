122069424a4aa5f6116b4d4725f3fffcc7068989
MiheerTamkhane <sarangtamkhane10@gmail.com>
2024-03-28 19:51:51 +0530

    Completed the TODO app in question 2

--- src/components/cycleOne/Question2.tsx
+++ src/components/cycleOne/Question2.tsx
@@ -13,30 +13,64 @@ export default Question2;
 const TodoApp = () => {
   const data = [
     {
-      id: "c1",
-      brand: "Toyota",
-      make: "Corolla",
-      model: "2007",
+      id: 1,
+      text: "First Todo",
+      completed: true,
     },
     {
-      id: "c2",
-      brand: "Maruti Suzuki",
-      make: "Omni",
-      model: "2003",
-    },
-    {
-      id: "c3",
-      brand: "Hyundai",
-      make: "Santro",
-      model: "2005",
+      id: 2,
+      text: "Second Todo",
+      completed: false,
     },
   ];
-  const [todos, setTodos] = useState(data);
+  type Todo = {
+    id: number;
+    text: string;
+    completed: boolean;
+  };
+
+  const [todos, setTodos] = useState<Todo[]>(data);
+  const [input, setInput] = useState<string>("");
+  function handleAddTodo() {
+    const item = {
+      id: Math.random(),
+      text: input,
+      completed: false,
+    };
+    setTodos((prevTodos) => [item, ...prevTodos]);
+    setInput("");
+  }
+
+  function completeTodoHandler(id: number) {
+    setTodos((prevTodos) =>
+      prevTodos.map((item) =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  }
   return (
     <>
-      {todos.map((todo) => (
-        <li>
-          {todo.brand} - {todo.make} - {todo.model}
+      <input
+        type="text"
+        placeholder="Add Todos..."
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      {todos?.map((todo) => (
+        <li
+          key={todo.id}
+          style={todo.completed ? { textDecoration: "line-through" } : {}}
+        >
+          {todo.text}
+          <button
+            style={{ marginLeft: "5px" }}
+            onClick={() => completeTodoHandler(todo.id)}
+          >
+            {todo.completed ? "Incomplete" : "Complete"}
+          </button>
         </li>
       ))}
     </>
