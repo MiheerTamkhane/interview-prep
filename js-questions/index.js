5b8068797837bd53a2a95e9586b2960b531356f7
MiheerTamkhane <sarangtamkhane10@gmail.com>
2024-05-16 16:11:16 +0530

    testing questions

--- js-questions/index.js
+++ js-questions/index.js
@@ -1,1 +1,27 @@
 // questions will fall in this section
+
+const users = [
+  {
+    id: 1,
+    name: "John",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Mira",
+    age: 21,
+  },
+  {
+    id: 3,
+    name: "Devil",
+    age: 19,
+  },
+  {
+    id: 4,
+    name: "Jack",
+    age: 25,
+  },
+];
+
+const names = users.map((person) => person.name);
+console.log(names);
---
+++ js-questions/javascript.md
@@ -0,0 +1,4 @@
+# null vs undefined
+
+undefined is basically a variable which does not initialized with any value.
+null is actually a value given to a variable.
