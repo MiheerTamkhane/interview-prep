0646b974b90f4037186e4f4c4159adab1fd4b803
Miheer Tamkhane <sarangtamkhane10@gmail.com>
2024-07-04 19:52:21 +0530

    added the same function

--- js-questions/patterns/frequency_counter_pattern/same.js
+++ js-questions/patterns/frequency_counter_pattern/same.js
@@ -2,4 +2,14 @@ function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
+  for (let i = 0; i < arr1.length; i++) {
+    let square = arr1[i] * arr1[i];
+    if (square !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
+
+console.log(same([1, 2, 3], [1, 4, 9]));
+console.log(same([1, 2, 3], [1, 4, 7]));
